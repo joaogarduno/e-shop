d6b2e7e1fd359fe6468b5dc50d7c2285fef4a21b
Joao Garduño <joao.garduno@gmail.com>
2024-01-29 12:34:39 -0600

    Estructura 2: Estilo y animación del menú

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css';
-import { logo } from "../Assets/logo.png";
-import { cart-icon } from "../Assets/cart_icon.png";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState('shop')
+
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -12,15 +15,16 @@ const Navbar = () => {
       </div>
 
       <ul className="nav-menu">
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={() => {setMenu('shop')}}>Shop {menu === 'shop' ? <hr/> : <></>}</li>
+        <li onClick={() => {setMenu('mens')}}>Men {menu === 'mens' ? <hr/> : <></>}</li>
+        <li onClick={() => {setMenu('womens')}}>Women {menu === 'womens' ? <hr/> : <></>}</li>
+        <li onClick={() => {setMenu('kids')}}>kids {menu === 'kids' ? <hr/> : <></>}</li>
       </ul>
 
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="Cart_Shopping_Icon" />
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   )
